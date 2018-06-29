9780af69439cb81dbc627f3e5ede6308d1638cf3
Joel Franusic <joel.franusic@okta.com>
2018-06-29 16:39:48 -0700

    Fix hard-coded URL

--- static/js/app-links.js
+++ static/js/app-links.js
@@ -1,48 +1,41 @@
-// FIXME: Make dynamic
-var org = "live-widget.oktapreview.com";
-
-var base_url = 'https://' + org;
-
 function showMyAppLinks(userId) {
-  // var userId = "00u121yglpifAyzNc1e8";
-  var userId = "00uexr1n7ybtpCkYB0h7";
-
-    if (userId != null && userId != '') {
-        url = base_url + "/api/v1/users/" + userId + "/appLinks";
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", url, true);
-        xhttp.withCredentials = true;
-        xhttp.send();
-        xhttp.onreadystatechange = function() {
-            var res = xhttp.responseText;
-            if (res) {
-                var linksJson = JSON.parse(res);
-                if (linksJson) {
-                    document.getElementById("my_links").style.display = 'block';
-                    var apps = new Vue({
-                        delimiters: ['[[', ']]'],
-                        el: '#vueapp',
-                        data: {
-                            appLinks: linksJson
-                        }
-                    });
-                    //drawTable(linksJson);
-                }
+  // FIXME: We shouldn't assume that localConfig has the value ...
+  var baseUrl = localConfig.page.okta.baseUrl;
+  signInWidget.session.get(function(sess){
+    var xhttp = new XMLHttpRequest();
+    url = baseUrl + "/api/v1/users/" + sess.userId + "/appLinks";
+    xhttp.open("GET", url, true);
+    xhttp.withCredentials = true;
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      var res = xhttp.responseText;
+      if (res) {
+        var linksJson = JSON.parse(res);
+        if (linksJson) {
+          document.getElementById("my_links").style.display = 'block';
+          var apps = new Vue({
+            delimiters: ['[[', ']]'],
+            el: '#vueapp',
+            data: {
+              appLinks: linksJson
             }
+          });
         }
+      }
     }
+  });
 }
 
 function drawTable(tableData) {
-    var i;
-    var draw = "";
-    for(i = 0; i < tableData.length; i++) {
-        draw += '<tr><td>' +
-        '<a href="' + tableData[i].linkUrl + '" target="_blank">' +
-        '<img src="' + tableData[i].logoUrl + '" class="img-rounded">' +
-        '<label>&nbsp;&nbsp;' + tableData[i].label + '</label>' +
-        '</a>' +
-        '</td></tr>';
-    }
-    document.getElementById("my_links").innerHTML = draw;
+  var i;
+  var draw = "";
+  for(i = 0; i < tableData.length; i++) {
+    draw += '<tr><td>' +
+      '<a href="' + tableData[i].linkUrl + '" target="_blank">' +
+      '<img src="' + tableData[i].logoUrl + '" class="img-rounded">' +
+      '<label>&nbsp;&nbsp;' + tableData[i].label + '</label>' +
+      '</a>' +
+      '</td></tr>';
+  }
+  document.getElementById("my_links").innerHTML = draw;
 }
